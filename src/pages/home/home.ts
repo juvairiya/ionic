059581958ab23a6cb7f56d8c611c1f9cd2e70bad
farhana <oliyathul@gmail.com>
2017-09-08 12:12:07 +0530

    Add ionic new project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ public items : any = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public http   : Http) {
 
   }
+  ionViewWillEnter()
+   {
+      this.load();
+   }
+   load()
+   {
+      this.http.get('http://localhost:8012/ionic/retrieve-data.php')
+      .map(res => res.json())
+      .subscribe(data =>
+      {
+         this.items = data;
+      });
+   }
+
+
+   // Allow navigation to the AddTechnology page for creating a new entry
+   addEntry()
+   {
+      this.navCtrl.push('AddTechnologyPage');
+   }
+
+
+   // Allow navigation to the AddTechnology page for amending an existing entry
+   // (We supply the actual record to be amended, as this method's parameter,
+   // to the AddTechnology page
+   viewEntry(param)
+   {
+      this.navCtrl.push('AddTechnologyPage', param);
+   }
+
 
 }
